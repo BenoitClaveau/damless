40b9c627a97f53eaca47742a75f2b3e84d33c43c
Benoît Claveau <benoit.claveau@izzili.com>
2018-12-10 11:58:40 +0100

    Add unit test for streamify

---
+++ tests/core/streamify.spec.js
@@ -0,0 +1,37 @@
+/*!
+ * damless
+ * Copyright(c) 2018 Benoît Claveau <benoit.claveau@gmail.com>
+ * MIT Licensed
+ */
+
+const expect = require("expect.js");
+const {
+    streamify,
+    getAll
+} = require("../../lib/core");
+
+describe("streamify", () => {
+
+    it("streamify a function", async () => {
+        const stream = streamify(() => {
+            return { value: 1 };
+        });
+
+        const all = await getAll(stream);
+        expect(all).to.eql({
+            value: 1
+        });
+    });
+
+    it("streamify an array", async () => {
+        const stream = streamify([1,2,3]);
+        const all = await getAll(stream);
+        expect(all).to.eql([1,2,3]);
+    });
+
+    it("streamify an object", async () => {
+        const stream = streamify({ value: 1 });
+        const all = await getAll(stream);
+        expect(all).to.eql([{ value: 1 }]);
+    });
+})
