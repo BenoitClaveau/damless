268f76bba2483f5ac7ee89008e03f9acbbf1fb6e
Benoît Claveau <benoit.claveau@izzili.com>
2018-12-13 12:32:09 +0100

    stream-flow

--- tests/core/stream-flow.spec.js
+++ tests/core/stream-flow.spec.js
@@ -5,44 +5,82 @@
  */
 
 const expect = require("expect.js");
+const fs = require("fs");
 const {
     ArrayToStream,
     StreamFlow,
     getAll,
     ending
 } = require("../../lib/core");
+const {
+    JsonStream,
+    Json
+} = require("../../lib/services/core");
+
 const { Transform } = require('stream');
 
 describe("stream-flow", () => {
 
     it("create a StreamFlow", async () => {
-        const stream = new ArrayToStream(["Execute multiples", "pipes inside", "a stream"]);
+        const transform1 = new Transform({
+            objectMode: true,
+            transform(chunk, encoding, callback) {
+                callback(null, {
+                    ...chunk,
+                    key: chunk.key.toUpperCase()
+                });
+            }
+        })
+
+        const transform2 = new Transform({
+            objectMode: true,
+            transform(chunk, encoding, callback) {
+                callback(null, {
+                    ...chunk,
+                    key: chunk.key.replace(/ /g, ";")
+                });
+            }
+        })
+
         const flow = new StreamFlow(
             stream => {
                 return stream
-                    .pipe(new Transform({
-                        objectMode: true,
-                        transform(chunk, encoding, callback) {
-                            callback(null, chunk.toUpperCase());
-                        }
-                    }))
-                    .pipe(new Transform({
-                        objectMode: true,
-                        transform(chunk, encoding, callback) {
-                            callback(null, chunk.replace(/ /g, ";"));
-                        }
-                    }))
+                    .pipe(transform1)
+                    .pipe(transform2)
             }
         );
 
-
-        const all = await getAll(stream.pipe(flow));
-        expect(all).to.eql([
-            "EXECUTE;MULTIPLES",
-            "PIPES;INSIDE",
-            "A;STREAM"
-        ]);
-    });
+        let cpt = 0;
+        const all = await getAll(
+            fs.createReadStream(`${__dirname}/../data/npm.array.json`)
+                .pipe(new JsonStream(new Json()).parse())
+                .pipe(flow)
+                .on("data", data => {
+                    cpt++;
+                    if (cpt == 100) {
+                        transform1.pause();
+                        setTimeout(() => {
+                            transform1.resume();
+                        }, 3000);
+                    }
+                })
+        )
+        expect(all.length).to.be(4028);
+        expect(all[1]).to.eql({
+            id: "3scale",
+            key: "3SCALE",
+            value: {
+                rev: "3-db3d574bf0ecdfdf627afeaa21b4bdaa"
+            }
+        });
+        expect(all[4027]).to.eql({
+            id: "zutil",
+            key: "ZUTIL",
+            value: {
+                rev: "9-3e7bc6520008b4fcd5ee6eb9e8e5adf5"
+            }
+        });
+    }).timeout(60000);
 
     it("throw an error in a StreamFlow", async () => {
         const stream = new ArrayToStream(["Execute multiples", "pipes inside", "a stream"]);
@@ -67,5 +105,7 @@ describe("stream-flow", () => {
             expect(error.message).to.be("Test");
         }
     });
+
+
 
 })
